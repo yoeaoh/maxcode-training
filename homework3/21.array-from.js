24e518866308c524334917f52662f0d9c522beab
yoeaoh <yoeaoh@gmail.com>
2025-08-06 00:24:53 +0300

    homework3 prepared 21

---
+++ homework3/21.array-from.js
@@ -0,0 +1,11 @@
+// https://maxcode.dev/problems/array-from/
+
+Array.from2 = function() {
+
+};
+
+const map = new Map();
+map.set(1, "a");
+map.set(2, "b");
+
+console.log(Array.from(map)); // [[1, "a"], [2, "b"]]

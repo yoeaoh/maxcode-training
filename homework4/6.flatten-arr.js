a73a3d5e5e49327b9bcfa137eca80c333314fd86
yoeaoh <yoeaoh@gmail.com>
2025-08-14 03:51:42 +0300

    homework4 6 done

--- homework4/6.flatten-arr.js
+++ homework4/6.flatten-arr.js
@@ -1,15 +1,36 @@
 // https://maxcode.dev/problems/flatten-arr/
 
-function flattenArr(arr, depth) {
+function flattenArr(arr, depth = 1, currentDepth = 0) {
+    if (!Array.isArray(arr)) {
+        return arr;
+    }
 
+    let resultArray = [];
+
+    const newDepth = currentDepth + 1;
+
+    for (const item of arr) {
+        if (currentDepth < depth) {
+            const flattenItem = flattenArr(item, depth, newDepth);
+
+            resultArray = resultArray.concat(flattenItem);
+
+            continue
+        }
+
+        resultArray.push(item);
+    }
+
+    return resultArray;
 }
 
 const x = [1, [[2, [3, 4, [5]], 6], [7]], [8]];
 
-flattenArr(x) === [1, [2, [3, 4, [5]], 6], [7], 8];
+console.dir(x, {depth: null});
+console.dir(flattenArr(x, 2), {depth: null})
 
-flattenArr(x, 0) === [1, [[2, [3, 4, [5]], 6], [7]], [8]]
+// console.log(flattenArr(x, 0) === [1, [[2, [3, 4, [5]], 6], [7]], [8]])
 
-flattenArr(x, 2) === [1, 2, [3, 4, [5]], 6, 7, 8]
+// console.log(flattenArr(x, 2) === [1, 2, [3, 4, [5]], 6, 7, 8])
 
-flattenArr(x, Infinity) === [1, 2, 3, 4, 5, 6, 7, 8]
+// console.log(flattenArr(x, Infinity) === [1, 2, 3, 4, 5, 6, 7, 8])
--- homework4/homework4.md
+++ homework4/homework4.md
@@ -9,7 +9,7 @@
 - [x] [3.tree-max-path](3.tree-max-path.js)
 - [ ] [4.deep-compare](4.deep-compare.js)
 - [x] [5.smart-sum](5.smart-sum.js)
-- [ ] [6.flatten-arr](6.flatten-arr.js)
+- [x] [6.flatten-arr](6.flatten-arr.js)
 - [x] [7.clone](7.clone.js)
 - [x] [8.permutations](8.permutations.js)
 - [x] [9.breadcrumbs](9.breadcrumbs.js)

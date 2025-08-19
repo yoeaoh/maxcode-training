35d370fd82c656b3cf79102e39fd09bd96957f2c
yoeaoh <yoeaoh@gmail.com>
2025-08-20 01:30:04 +0300

    homework5 4 done

--- homework5/4.count-fulfilled-promises.js
+++ homework5/4.count-fulfilled-promises.js
@@ -1,16 +1,24 @@
 // https://maxcode.dev/problems/count-fulfilled-promises/
 
-function countFulfilledPromises() {
-
+function countFulfilledPromises(...promises) {
+    return promises.reduce((acc, promise) => {
+        return promise
+            .then(() => acc.then(prevValue => prevValue + 1))
+            .catch(() => acc.then(prevValue => prevValue))
+    }, Promise.resolve(0))
 }
 
 const p1 = new Promise(resolve => resolve(1));
 const p2 = new Promise(resolve => resolve(2));
-const p3 = new Promise((_, reject) => reject(3));
+// Если запустить код, в котором будет эта строка (14) и строка (20)
+// const p3 = new Promise((_, reject) => reject(3));
 
-countFulfilledPromises().then(console.log);            // 0
-countFulfilledPromises(p1).then(console.log);          // 1
+// countFulfilledPromises().then(console.log);            // 0
+// countFulfilledPromises(p1).then(console.log);          // 1
+
+// И эта строка (20) -- то он крашится.. если закомментить строку 14, то всё окей.
 countFulfilledPromises(p1, p2).then(console.log);      // 2
-countFulfilledPromises(p1, p2, p3).then(console.log);  // 2
-countFulfilledPromises(p1, p3).then(console.log);      // 1
-countFulfilledPromises(p3).then(console.log);          // 0
+
+// countFulfilledPromises(p1, p2, p3).then(console.log);  // 2
+// countFulfilledPromises(p1, p3).then(console.log);      // 1
+// countFulfilledPromises(p3).then(console.log);          // 0
--- homework5/homework5.md
+++ homework5/homework5.md
@@ -5,5 +5,5 @@
 
 - [x] [1.add-two-promises](1.add-two-promises.js)
 - [x] [need to refactor] [2.add-all-promises](2.add-all-promises.js)
-- [x] [3.promise-logic](3.promise-logic.js)
-- [ ] [4.count-fulfilled-promises](4.count-fulfilled-promises.js)
+- [x] [need some explanation, or try different approach] [3.promise-logic](3.promise-logic.js)
+- [x] [4.count-fulfilled-promises](4.count-fulfilled-promises.js)

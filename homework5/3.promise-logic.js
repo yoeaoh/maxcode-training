9fdf7c80c16c344a81837524c4cd6ccdd904b7f0
yoeaoh <yoeaoh@gmail.com>
2025-08-20 01:11:24 +0300

    homework5 3 done (but needs epxlanation)

--- homework5/3.promise-logic.js
+++ homework5/3.promise-logic.js
@@ -1,7 +1,10 @@
 // https://maxcode.dev/problems/promise-logic/
 
+// Нужно объяснение, написал наугад (я вообще думал, что
+// resoolve/reject строку принимают, но решил попробовать
+// передать туда промис)
 function and(p1, p2) {
-
+    return p1.then(() => Promise.resolve(p2)).catch(() => Promise.reject(p2));
 }
 
 and(Promise.resolve(1), Promise.resolve(2)).then(

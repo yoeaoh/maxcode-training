eb7c72b8a8cbed9ccf773b86260f6fe02f09190d
yoeaoh <yoeaoh@gmail.com>
2025-08-26 01:27:02 +0300

    homework5 3 added second variant

--- 6.homework5/3.promise-logic.js
+++ 6.homework5/3.promise-logic.js
@@ -1,7 +1,18 @@
 // https://maxcode.dev/problems/promise-logic/
 
-function and(p1, p2) {
+// v1
+function and1(p1, p2) {
     return p1.then(() => new Promise(res => res(p2))).catch(() => new Promise((_, rej) => rej(p2)));
+}
+
+// v2
+function and(p1, p2) {
+    return p1
+        .then(() => p2
+            .then(value => new Promise(res => res(value)))
+            .catch(reason => new Promise((_, rej) => rej(reason))
+        )
+        .catch((reason) => new Promise((_, rej) => rej(reason))))
 }
 
 and(Promise.resolve(1), Promise.resolve(2)).then(

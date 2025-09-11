f375eead31155c41d7e9707b09d22af3fb1c4d69
yoeaoh <yoeaoh@gmail.com>
2025-09-11 23:55:33 +0300

    homework5 16 done

--- 6.homework5/16.chain.js
+++ 6.homework5/16.chain.js
@@ -1,33 +1,36 @@
 // https://maxcode.dev/problems/chain/
 
-// TODO: Какие числа выведутся в консоль?
+// Вроде лёгкая, хотя с непривычки можно начать кетчи учитывать/
+function solution() {
+    return [6, 340];
+}
 
 Promise.resolve(1)
-    .then(x => x * 2)
-    .then(x => x * 3, x => x + 5)
-    .catch(x => x + 10)
+    .then(x => x * 2) // 2
+    .then(x => x * 3, x => x + 5) // 6
+    .catch(x => x + 10) // skip, nothing to catch
     .then(x => {
-        console.log(x);
-        return x + 1;
+        console.log(x); // 6, console 1
+        return x + 1; // 7
     })
     .then(x => {
-        throw x * 2;
+        throw x * 2; // 14
     })
-    .then(x => x * 4)
-    .then(x => x + 1, x => x + 3)
+    .then(x => x * 4) // skip
+    .then(x => x + 1, x => x + 3) // 17, first catch after throw
     .catch(x => {
-        console.log(x);
+        console.log(x); // skip
         throw x + 30;
     })
     .catch(x => {
-        console.log(x);
+        console.log(x); // skip
         throw x + 50;
     })
-    .then(x => x * 20)
+    .then(x => x * 20) // 17 * 20 = 20 * 20 - 60 = 340
     .catch(x => {
         console.log(x);
         return x + 3
     })
     .then(x => {
-        console.log(x);
+        console.log(x); // console 2, 340
     });

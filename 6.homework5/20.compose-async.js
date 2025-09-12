d29b69e365df12ca7a659970bee8f0acbfc99a1b
yoeaoh <yoeaoh@gmail.com>
2025-09-12 03:22:37 +0300

    homework5 20 attempted, almost done

--- 6.homework5/20.compose-async.js
+++ 6.homework5/20.compose-async.js
@@ -1,7 +1,50 @@
 // https://maxcode.dev/problems/compose-async/
 
-function compose(fns) {
+// Это чёт не проходит, хотя вроде мапу добавил
 
+// 04. Для одинакового аргумента функция возвращает один и тот же ответ
+//     Promise resolution is still pending but the event loop has already resolved
+
+function compose(fns) {
+    const functionsCount = fns.length;
+    let composedFns = 0;
+
+    const resultsMap = new Map()
+
+    return function(initialValue) {
+        return new Promise((resolve, reject) => {
+            if (functionsCount === 0) {
+                resolve(initialValue);
+            }
+
+            return fns.reduceRight((acc, fn) => {
+                return acc.then(accValue => {
+                    console.log(resultsMap)
+
+                    if (resultsMap.has(fn)) {
+                        if (resultsMap.get(fn)[accValue]) {
+                            return Promise.resolve(resultsMap.get(fn)[accValue])
+                        }
+                    }
+
+                    return fn(accValue).then(
+                            value => {
+                                resultsMap.set(fn, {[accValue]: value})
+
+                                composedFns += 1;
+
+                                if (composedFns === functionsCount) {
+                                    resolve(value);
+                                }
+
+                                return value
+                            })
+                    },
+                    reason => reject(reason)
+                )
+            }, Promise.resolve(initialValue))
+        })
+    }
 }
 
 const square = x => new Promise(r => setTimeout(r, 2000, x ** 2));

edf53e271edc1558402fd95c8e897ad624fde9a7
yoeaoh <yoeaoh@gmail.com>
2025-09-13 16:58:24 +0300

    homework5 22 done

--- 5.homework5/22.promise-batch.js
+++ 5.homework5/22.promise-batch.js
@@ -1,10 +1,22 @@
 // https://maxcode.dev/problems/promise-batch/
 
-function run(fns, limit) {
-    let runningFns = 0;
+// TODO: maybe refactor
+async function run(fns, limit) {
+    const result = [];
+    let from = 0;
+    let to = limit;
 
-    const firstPortion = fns.slice(0, limit);
-    console.log(firstPortion);
+    return new Promise(async (resolve) => {
+        while (to - limit < fns.length) {
+            const portion = fns.slice(from, to).map(fn => fn());
+            from += limit;
+            to += limit;
+
+            await Promise.all(portion).then(values => result.push(...values));
+        }
+
+        resolve(result);
+    })
 }
 
 const fn1 = () => new Promise(r => setTimeout(r, 3400, "a"));
@@ -15,6 +27,7 @@ const fn5 = () => new Promise(r => setTimeout(r, 1800, "e"));
 const fn6 = () => new Promise(r => setTimeout(r, 400, "f"));
 
 
-run([fn1, fn2, fn3, fn4, fn5, fn6], 2).then(arr => {
+run([fn1, fn2, fn3, fn4, fn5, fn6], 2)
+.then(arr => {
     console.log(arr); // arr === ["a", "b", "c", "d", "e", "f"]
 });
